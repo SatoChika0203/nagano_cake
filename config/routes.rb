a20932df42cd9f8e15d7132896d4f0a59ccf3cb4
EC2 Default User <ec2-user@ip-172-31-1-135.ap-northeast-1.compute.internal>
2023-05-12 13:23:47 +0000

    [Add]コントローラー、データベース作成

---
+++ config/routes.rb
@@ -0,0 +1,25 @@
+Rails.application.routes.draw do
+  devise_for :admins, skip: [:registrations, :passwords], controllers: {
+    sessions: 'admin/sessions'
+  }
+  # 登録とパスワード変更のルーティングを削除する
+  
+  devise_for :customers,skip: [:passwords], controllers: {
+    registration: "public/registrations",
+    sessions: 'public/sessions'
+  }
+  
+  scope module: :public do
+    resources :homes
+    resources :items
+    resources :cart_items
+  end
+  # module: URLそのままで、ファイル構成（アクションとコントローラー）のみ指定のパスにしたい
+  
+  namespace :admin do
+    resources :homes
+    resources :items
+  end
+  # URLを分けるにはnamespaceを使用する
+  # 会員側と管理者側によって、ルーティング(URL 等)をグループ分けして管理する
+end
