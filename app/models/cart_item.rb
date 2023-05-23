12ba86e84e81216952cbc978175ebfe98d07357b
EC2 Default User <ec2-user@ip-172-31-1-135.ap-northeast-1.compute.internal>
2023-05-23 15:37:29 +0000

    [Add]カート内の削除（単体）

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -2,4 +2,10 @@ class CartItem < ApplicationRecord
   belongs_to :customer
   belongs_to :item
   # belongs_toはsつけない
+  
+  def subtotal
+    item.with_tax_price * amount  #小計を求めるメソッド
+  end
+  # カート機能で使用するメソッドのためカートモデルに記述
+  # 消費税を求めるメソッドを呼び出した上で算出する
 end
