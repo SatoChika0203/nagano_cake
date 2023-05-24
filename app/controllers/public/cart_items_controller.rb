e2c18359e06e225c69a6ea21fdb458393bae1940
EC2 Default User <ec2-user@ip-172-31-1-135.ap-northeast-1.compute.internal>
2023-05-24 14:41:25 +0000

    [Add]複数商品をカートに入れた際の挙動

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -20,14 +20,25 @@ class Public::CartItemsController < ApplicationController
 
   def create
     @cart_item=CartItem.new(cart_item_params)
-    if @cart_item.save
+    cart_item=CartItem.find_by(item_id: params[:cart_item][:item_id])
+    #CariItemモデルから生成されたインスタンスの中に、cart_itemのitem_idがあるかどうか
+    if cart_item.present?
+     #cart_itemのitem_idがあるかどうか？
+      cart_item.amount += @cart_item.amount.to_i
+      cart_item.save
+      redirect_to cart_items_path
+    elsif @cart_item.save
+      @cart_item=CartItem.all
       redirect_to cart_items_path
     else
-      @item=Item.find(params[:cart_item][:item_id])
-      # cart_itemのitem_id
       render 'public/items/show'
     end
   end
+  
+
+
+  
+
 
 private
   def cart_item_params
