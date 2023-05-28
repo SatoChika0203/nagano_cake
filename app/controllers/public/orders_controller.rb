daf5eca5525a777acd581a01b7373e118721f588
EC2 Default User <ec2-user@ip-172-31-1-135.ap-northeast-1.compute.internal>
2023-05-28 15:57:04 +0000

    [Add]注文確認画面の支払方法の記述

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -6,8 +6,10 @@ end
 
 def confirm
   @cart_items=CartItem.all
-  @payment_method=Order.payment_methods_i18n[:payment_method]
-  @address=params[:order][:address]
+  
+  order=Order.new(order_params)   #order_paramsの値が入ったインスタンスを生成する
+  @payment_method=order.payment_method_i18n
+  # @address=params[:order][:address]
   render :confirm
   # redirect_to:もう一度ルーティングからやり直して画面表示・・変数の値も消えてしまう
   # render：そのまま画面を移行する・・変数の値そのまま
@@ -19,7 +21,7 @@ end
 
 private
   def order_params
-     params.require(:order).permit(:payment_method, :address)
+     params.require(:order).permit(:payment_method)
   end
   #order_paramsの各カラムの情報が入ったインスタンスを、新しく生成
 end
